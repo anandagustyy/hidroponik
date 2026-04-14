aceec06ee177a3813be7c79b07ced58c61ce872d
Ananda Gusty <nuelagustian123@gmail.com>
2026-04-14 11:00:28 +0700

    update dashboard

---
+++ ESP32Hidroponik/ESP32Hidroponik.ino
@@ -0,0 +1,51 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+// WIFI
+const char* ssid = "anandagusty";
+const char* password = "anakbaik";
+
+// FIREBASE
+String firebaseURL = "https://hidroponik-4c359-default-rtdb.asia-southeast1.firebasedatabase.app/sensor.json";
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi Connected");
+}
+
+void loop() {
+  float ph = analogRead(34) * (14.0 / 4095.0);
+  float ppm = analogRead(35) * (1000.0 / 4095.0);
+
+  Serial.print("pH: ");
+  Serial.print(ph);
+  Serial.print(" | PPM: ");
+  Serial.println(ppm);
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    http.begin(firebaseURL);
+    http.addHeader("Content-Type", "application/json");
+
+    String json = "{\"ph\":" + String(ph, 2) + ",\"ppm\":" + String(ppm, 0) + "}";
+
+    int response = http.PUT(json);
+
+    Serial.print("Response: ");
+    Serial.println(response);
+
+    http.end();
+  }
+
+  delay(5000);
+}
